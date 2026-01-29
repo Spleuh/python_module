897a612a89951520045dc05e53eed93303824880
Jean-Yves Sam <jsam@z3r12p5.42lyon.fr>
2026-01-29 17:22:27 +0100

    retry, modify ex7 and delete header for norm

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary():
+    name = input("Enter garden name: ")
+    nbplants = int(input("Enter number of plants: "))
+    print("Garden: ", name)
+    print("Plants: ", nbplants)
+    print("Status: Growing well!")
