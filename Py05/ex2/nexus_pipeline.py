5be5eca0ae9a7ebbaef98240d2c6aca92f68dd71
jsam <Spleuh@gmail.com>
2026-03-03 23:11:05 +0100

    the JSON adapter has advanced the responsibilities of the classes are separate but the logical part (class stages) can be a little more diluted in several methods

--- Py05/ex2/nexus_pipeline.py
+++ Py05/ex2/nexus_pipeline.py
@@ -1,5 +1,16 @@
-from typing import Collection, Any, Protocol, Dict, Union
+from typing import Any, Protocol, Dict, Union
 from abc import ABC, abstractmethod
+from collections import defaultdict
+
+
+class ErrorJSON(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class ErrorStage(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
 
 
 class ProcessingStage(Protocol):
@@ -12,14 +23,40 @@ class InputStage:
         pass
 
     def process(self, data: Any) -> Dict:
-        return data
+        if isinstance(data, dict):
+            result = defaultdict(lambda: None, data)
+        else:
+            result = defaultdict(lambda: None, {key: None for key in data.split(",")})
+        return result
 
 
 class TransformStage:
     def __init__(self) -> None:
         pass
 
+    def validate_stream(self, data: dict) -> bool:
+        for data in data.items():
+            if "temp" not in data[0] or not isinstance(data[1], float):
+                return False
+        return True
+
+    def add_adapter(self, data: Any) -> Dict:
+        json_keys = {"sensor", "value", "unit"}
+        csv_keys = {"user", "action", "timestamp"}
+        data_keys = set(data.keys())
+        if len(json_keys.difference(data_keys)) == 0:
+            data["adapter"] = "json"
+        elif len(csv_keys.difference(data_keys)) == 0:
+            data["adapter"] = "csv"
+        elif self.validate_stream(data):
+            data["adapter"] = "stream"
+        else:
+            raise ErrorStage("type data unknown")
+        return data
+
     def process(self, data: Any) -> Dict:
+        data = self.add_adapter(data)
+        data["transformed"] = True
         return data
 
 
@@ -28,7 +65,17 @@ class OutputStage:
         pass
 
     def process(self, data: Any) -> str:
-        return data
+        result = ""
+        if data["adapter"] == "json":
+            temp = data['value']
+            result = f"Output: Processed temperature reading: {temp}°{data['unit']} {'(Normal range)' if isinstance(temp, float) and temp < 32 and temp > 12 else ''}"
+        elif data["adapter"] == "cvs":
+            result = "Output: User activity logged: "
+        elif data["adapter"] == "stream":
+            result = "Output: Stream summary: "
+        else:
+            raise ErrorStage(f"Error Outpustage: adapter unknown: {data.get('adapter')}")
+        return result
 
 
 class ProcessingPipeline(ABC):
@@ -53,7 +100,7 @@ class JSONAdapter(ProcessingPipeline):
         return data
 
 
-class CVSAdapter(ProcessingPipeline):
+class CSVStream(ProcessingPipeline):
     def __init__(self):
         super().__init__()
 
@@ -86,4 +133,10 @@ class NexusManager:
 
 
 if __name__ == "__main__":
-    pass
+    jtest = JSONAdapter()
+    jtest.add_stage(InputStage())
+    jtest.add_stage(TransformStage())
+    jtest.add_stage(OutputStage())
+    data_test = {"sensor": "temp", "value": 23.5, "unit": "C"}
+    str_test = jtest.process(data_test)
+    print(str_test)
