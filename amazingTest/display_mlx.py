d2b6b86d710b9e86d9936c66605626f0b5cfe463
jsam <Spleuh@gmail.com>
2026-03-03 01:11:06 +0100

    delete test

--- amazingTest/display_mlx.py
+++
@@ -1,7 +0,0 @@
-import mlx
-
-if __name__ == "__main__":
-    ptrmlx = mlx.Mlx().mlx_get_data_addr()
-    ptrwin = mlx.Mlx().mlx_new_window(ptrmlx, 600, 300, "test")
-    test = input()
-    ptrmlx.mlx_destroy_window(ptrmlx, ptrwin)
