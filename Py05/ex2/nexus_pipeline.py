5408746b2ee5e09569d6f66b24c0754c1e0b408b
jsam <Spleuh@gmail.com>
2026-03-04 12:42:44 +0100

    restruct program not finished

--- Py05/ex2/nexus_pipeline.py
+++ Py05/ex2/nexus_pipeline.py
@@ -26,7 +26,8 @@ class InputStage:
         if isinstance(data, dict):
             result = defaultdict(lambda: None, data)
         else:
-            result = defaultdict(lambda: None, {key: None for key in data.split(",")})
+            result = defaultdict(lambda: None,
+                                 {key: None for key in data.split(",")})
         return result
 
 
@@ -35,8 +36,8 @@ class TransformStage:
         pass
 
     def validate_stream(self, data: dict) -> bool:
-        for data in data.items():
-            if "temp" not in data[0] or not isinstance(data[1], float):
+        for d in data.items():
+            if "temp" not in d[0] or not isinstance(d[1], float):
                 return False
         return True
 
@@ -66,15 +67,33 @@ class OutputStage:
 
     def process(self, data: Any) -> str:
         result = ""
-        if data["adapter"] == "json":
-            temp = data['value']
-            result = f"Output: Processed temperature reading: {temp}°{data['unit']} {'(Normal range)' if isinstance(temp, float) and temp < 32 and temp > 12 else ''}"
-        elif data["adapter"] == "cvs":
-            result = "Output: User activity logged: "
-        elif data["adapter"] == "stream":
-            result = "Output: Stream summary: "
+        if data['transformed']:
+            if data["adapter"] == "json":
+                result = self.format_json(data)
+            elif data["adapter"] == "cvs":
+                result = "Output: User activity logged: "
+            elif data["adapter"] == "stream":
+                result = "Output: Stream summary: "
+            else:
+                raise ErrorStage("Error Outpustage: adapter unknown: "
+                                 f"{data.get('adapter')}")
         else:
-            raise ErrorStage(f"Error Outpustage: adapter unknown: {data.get('adapter')}")
+            raise ErrorStage(f"Error Stage 2, data not transformed: {data}")
+        return result
+
+    def format_json(self, data: Any) -> str:
+        temp = data['value']
+        unit = data['unit']
+        if not isinstance(temp, float):
+            raise ErrorStage(f"Error type: temp {temp}")
+        if 12 < temp < 32:
+            range = "(Normal range)"
+        else:
+            range = ""
+        if unit not in ["K", "C", "F"]:
+            raise ErrorStage(f"Error type: unit {unit}")
+        result = ("Output: Processed temperature reading: "
+                  f"{temp}°{unit} {range}")
         return result
 
 
@@ -87,17 +106,54 @@ class ProcessingPipeline(ABC):
 
     @abstractmethod
     def process(self, data: Any) -> Any:
-        pass
+        for stage in self.stages:
+            data = stage.process(data)
+
+
+def convert(value: Any) -> str | int | float:
+    try:
+        return (int(value))
+    except ValueError:
+        try:
+            return (float(value))
+        except ValueError:
+            try:
+                return (str(value))
+            except Exception as e:
+                print(f"{e}")
 
 
 class JSONAdapter(ProcessingPipeline):
     def __init__(self):
         super().__init__()
 
+    def validate(self, tmp: list[list[str]]) -> bool:
+        for i in tmp:
+            for j in i:
+                if not isinstance(j[0], str):
+                    return False
+        return True
+
+    def pre_process(self, data: Any):
+        result: dict[str: str | float] = {}
+        if isinstance(data, str):
+            tmp = data.split(",")
+            tmp = [splited for t in tmp for splited in [t.split(":")]]
+            if not self.validate(tmp):
+                raise ErrorJSON("Data not formated, type value != str "
+                                f"or float {tmp}")
+            result = {str(kv[0]): str(kv[1]) for kv in tmp}
+            print(type(result))
+            print(result)
+
+    def post_process(self, data: Any):
+        pass
+
     def process(self, data: Any) -> Union[str, Any]:
-        for stage in self.stages:
-            data = stage.process(data)
-        return data
+        data = self.pre_process(data)
+        result = super().process(data)
+        result = self.post_process(result)
+        return result
 
 
 class CSVStream(ProcessingPipeline):
@@ -138,5 +194,6 @@ if __name__ == "__main__":
     jtest.add_stage(TransformStage())
     jtest.add_stage(OutputStage())
     data_test = {"sensor": "temp", "value": 23.5, "unit": "C"}
-    str_test = jtest.process(data_test)
+    str_test = "sensor:temp,value:23.5,unit:C"
+    str_test = jtest.process(str_test)
     print(str_test)
