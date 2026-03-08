22fd6839d66326b5bf13452ae016a393f6998af7
Jean-Yves Sam <jsam@z4r8p5.42lyon.fr>
2026-03-08 18:40:07 +0100

    import module started, elements and potions ok

---
+++ alchemy/potions.py
@@ -0,0 +1,22 @@
+from .elements import create_air, create_earth, create_fire, create_water
+
+
+def healing_potion():
+    return (f"Healing potion brewed with {create_fire()} and "
+            f"{create_water()}")
+
+
+def strength_potion():
+    return (f"Strength potion brewed with {create_earth()} and "
+            f"{create_fire()}")
+
+
+def invisibility_potion():
+    return (f"Invisibility potion brewed with {create_air()} and "
+            f"{create_water()}")
+
+
+def wisdom_potion():
+    return ("Wisdom potion brewed with all elements"
+            f"{create_water()} {create_air()}"
+            f"{create_fire()} {create_earth()}")
