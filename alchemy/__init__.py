f36b4e4ba3e1eed192396eb335e490777f229851
Jean-Yves Sam <jsam@z4r10p3.42lyon.fr>
2026-03-09 16:25:25 +0100

    correction try except + modify some acces import

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,16 +1,6 @@
 __version__ = "1.0.0"
 __author__ = "Master Pythonicus"
 
-from .elements import create_air, create_earth, create_fire, create_water
-from .potions import healing_potion
+from .elements import create_fire, create_water
 
-
-def main():
-    create_water()
-    create_fire()
-    create_earth()
-    create_air()
-
-
-def heal():
-    return healing_potion()
+__all__ = ["create_fire", "create_water", "healing_potion"]
