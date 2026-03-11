44d71ffd18c601fe2731f59d8d0f8936f6aa7a24
Jean-Yves Sam <jsam@z4r9p5.42lyon.fr>
2026-03-11 12:35:42 +0100

    fix  mypy init grim

--- Py06/alchemy/grimoire/__init__.py
+++ Py06/alchemy/grimoire/__init__.py
@@ -2,6 +2,4 @@ from .spellbook import record_spell
 from .validator import validate_ingredients
 
 
-if __name__ == '__main__':
-    record_spell()
-    validate_ingredients()
+__all__ = ['record_spell', 'validate_ingredients']
