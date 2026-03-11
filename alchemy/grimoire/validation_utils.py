c1b0b73c7beeb8929aef6c74d48d3ba1f95fbd87
Jean-Yves Sam <jsam@z4r9p5.42lyon.fr>
2026-03-11 12:22:14 +0100

    fix: error during read-tree

--- alchemy/grimoire/validation_utils.py
+++
@@ -1,6 +0,0 @@
-def is_valid(ingredients: str) -> bool:
-    elements = ['fire', 'water', 'earth', 'air']
-    for i in elements:
-        if i in ingredients:
-            return True
-    return False
--- alchemy/grimoire/validator.py
+++
@@ -1,8 +0,0 @@
-from .validation_utils import is_valid
-
-
-def validate_ingredients(ingredients: str) -> str:
-    if is_valid(ingredients):
-        return (f'{ingredients} - VALID')
-    else:
-        return (f'{ingredients} - INVALID')
