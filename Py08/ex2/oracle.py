d76ed40f01872bae6ceae6067c9af8451cde9a5a
jsam <Spleuh@gmail.com>
2026-03-22 13:48:42 +0100

    Py08:ex2: ex2 done, prog work but norm NOK

--- Py08/ex2/oracle.py
+++ Py08/ex2/oracle.py
@@ -19,6 +19,38 @@ def check_depend() -> bool:
     return True
 
 
+def check_conf(key: str) -> bool:
+    conf = os.getenv(key)
+    if not conf:
+        return False
+    return True
+
+
+def check_env() -> bool:
+    lst_key = ['MATRIX_MODE', 'DATABASE_URL', 'API_KEY', 'LOG_LEVEL', 'ZION_ENDPOINT']
+    result = all([os.getenv(key) for key in lst_key])
+    return result
+
+
+def print_info(mode: str):
+    if mode == 'development' or mode == 'production':
+        print(f"Database: {'DATABASE_URL is missing' if not os.getenv('DATABASE_URL') else 'Connected to local instance'}")
+        print(f"API Acces: {'API_KEY is missing' if not os.getenv('API_KEY') else 'Authentificated'}")
+        print(f"Log Level: {'LOG_LEVEL is missing' if not os.getenv('LOG_LEVEL') else 'DEBUG'}")
+        print(f"Zion Network: {'ZION_ENDPOINT is missing' if not os.getenv('ZION_ENDPOINT') else 'Online\n'}")
+    else:
+        print('Error: mode unknown\n')
+
+
+def security_check(mode: str):
+    prod = mode == 'production'
+    print('Environment security check:')
+    print('[OK] No hardcoded secrets detected')
+    env_check = check_env()
+    print(f"[{'OK' if env_check else 'KO'}] .env file {'not ' if not env_check else ''}properly configured")
+    print(f'[{'OK' if prod else 'KO'}] Production overrides {'not ' if not prod else ''}available')
+
+
 def main():
     if not safe_exec(check_depend):
         print('python-dotenv is missing: '
@@ -30,9 +62,15 @@ def main():
 
     dotenv.load_dotenv()
     print('Configuration loaded:')
-    print(f"Mode: {os.getenv('MATRIX_MODE')}")
-    print(f"Database: {os.getenv('DATABASE_URL')}")
 
+
+    mode = os.getenv('MATRIX_MODE')
+    print(f"Mode: {'MATRIX_MODE is missing' if not mode else mode}")
+    if mode:
+        print_info(mode)
+    
+    security_check(mode)
+    print('\nThe Oracle sees all configurations.')
 
 if __name__ == '__main__':
     main()
