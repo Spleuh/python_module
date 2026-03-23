cc228a436bb10a76255acf779c6c1d67c38a0b93
jsam <Spleuh@gmail.com>
2026-03-23 13:17:01 +0100

    Py09:ex1: init file alien_contact.py

---
+++ Py09/ex1/alien_contact.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from pydantic import BaseModel, model_validator, Field
+from datetime import datetime
+
+
+class ContactType(Enum):
+    RADIO = "radio"
+    VISUAL = "visual"
+    PHYSICAL = "physical"
+    TELEPATHIC = "telepathic"
+
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5, max_length=15)
+    timestamp: datetime
+    location: str = Field(min_length=3, max_length=100)
